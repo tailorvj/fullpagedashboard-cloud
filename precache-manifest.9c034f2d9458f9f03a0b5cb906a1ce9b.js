b9a23117aa2d59fcfb291e22786517a44a38b966
Stas Arshanski <stuk88@gmail.com>
2019-03-14 20:22:47 +0200

    Updates

---
+++ precache-manifest.9c034f2d9458f9f03a0b5cb906a1ce9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2c70b08adeb53c0abd26",
+    "url": "/static/js/main.2c70b08a.chunk.js"
+  },
+  {
+    "revision": "4c7f58ff8d07cc19c12d",
+    "url": "/static/js/1.4c7f58ff.chunk.js"
+  },
+  {
+    "revision": "2c70b08adeb53c0abd26",
+    "url": "/static/css/main.23715101.chunk.css"
+  },
+  {
+    "revision": "4c7f58ff8d07cc19c12d",
+    "url": "/static/css/1.a4342906.chunk.css"
+  },
+  {
+    "revision": "7d78b6379abe5b1cf6e92725726f611b",
+    "url": "/index.html"
+  }
+];
