b9f8710ffe73c28b0bf102781bc74342f7d0eb40
Ilana Hakim <ilana.hakim@gmail.com>
2019-04-12 09:02:36 +0300

    updated playlists view

--- src/containers/routes/playlists/PlaylistsList.js
+++ src/containers/routes/playlists/PlaylistsList.js
@@ -38,38 +38,61 @@ class PlaylistsList extends Component {
     const { playlists } = this.props;
     const myPlaylists = playlists.map((item) => {
         return(
-            <div className="item" key={item.playlistID}>
-                <div className="right floated content">
-                    <button 
-                        className="ui basic icon button"
-                        onClick={() => navigate(`/addURL/${this.props.userID}/${item.playlistID}`)}
-                    >
-                        <i className="icon edit"></i>
-                    </button>
-                    <button 
-                        className="ui basic icon button"
-                        onClick={() => navigate(`/URLs/${this.props.userID}/${item.playlistID}`)}
-                    >
-                        <i className="icon eye"></i>
-                    </button>
-                    <button 
-                        className="ui basic icon button"
-                        onClick={e => this.deletePlaylist(e, item.playlistID)}
-                    >
-                        <i className="icon delete"></i>
-                    </button>
-                </div>
-                <i className="middle aligned icon list ul "></i>
-                <div className="middle aligned content">
-                  {item.playlistName}
-                </div>
-            </div>
 
+            <div className="item" key={item.playlistID}>
+                <div className="right floated content ui basic icon buttons">
+                    <button className="ui link button" href="#"
+                        onClick={() => navigate(`/addURL/${this.props.userID}/${item.playlistID}`)}>
+                        <i className="large icons">
+                            <i className="fitted link  linkify icon"></i>
+                            <i className="plus corner icon"></i>
+                        </i>
+                    </button>
+                    <button className="ui link button" href="#"
+                        onClick={() => navigate(`/URLs/${this.props.userID}/${item.playlistID}`)}>
+                        <i className="icon eye large"></i>
+                    </button>
+                    <button className="ui link button" href="#"
+                        onClick={e => this.deletePlaylist(e, item.playlistID)}>
+                        <i className="icon delete large"></i>
+                    </button>
+                </div>
+                <div className="content" style={{paddingTop: .5 + 'em'}}>
+                    <h2 className="header">{item.playlistName}</h2>
+                </div>
+                {/*
+                            <div className="item" key={item.playlistID}>
+                                <div className="right floated content">
+                                    <a className="ui basic icon" href="#" alt="Add URL"
+                                       onClick={() => navigate(`/addURL/${this.props.userID}/${item.playlistID}`)}
+                                    >
+                                        <i className="large icons">
+                                          <i className="linkify icon"></i>
+                                          <i className="plus corner icon"></i>
+                                        </i>
+                                    </a>
+                                    <a className="ui basic icon" href="#" alt=""
+                                       onClick={() => navigate(`/URLs/${this.props.userID}/${item.playlistID}`)}
+                                    ><i className="icon eye large"></i></a>
+
+                                    <a className="ui basic icon" href="#"
+                                        onClick={e => this.deletePlaylist(e, item.playlistID)}
+                                    ><i className="icon delete large"></i></a>
+
+                                </div>
+                                <i className="middle aligned icon list ul "></i>
+                                <div className="middle aligned content">
+                                  {item.playlistName}
+                                </div>
+                            </div>
+                */}
+            </div>
 
         );
     });
     return (
-        <div className="ui middle aligned animated selection divided list">
+        <div className="ui animated selection relaxed divided list">
+        {/*<div className="ui middle aligned animated selection divided list">*/}
             {
                 this.state.errorMessage !== null ?
                     <FormError theMessage={this.state.errorMessage}/> 
